a517bddb6036de1ccd1db7f7143b0f60c2c100fa
Leonard Souza <hey@leonardsouza.com>
2016-06-11 08:27:19 -0700

    added initial scaffolding

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default class ReactAudio extends React.Component {
+  static propTypes = {
+    name: React.PropTypes.string,
+  };
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div></div>
+    );
+  }
+}
---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+const webpack = require('webpack');
+const path = require('path');
+const precss       = require('precss');
+const autoprefixer = require('autoprefixer');
+const simpleVars = require('postcss-simple-vars');
+const postcssMixins = require('postcss-mixins');
+const postcssBEM = require('postcss-bem')({style: 'bem'});
+const postcssNested = require('postcss-nested');
+const colorFunctions = require('postcss-color-function');
+const postcssImport = require('postcss-import');
+const customMedia = require('postcss-custom-media');
+
+const BUILD_DIR = path.resolve(__dirname, 'public');
+const APP_DIR = path.resolve(__dirname, 'src');
+
+const config = {
+  entry: APP_DIR + '/index.js',
+  output: {
+    path: BUILD_DIR,
+    filename: 'bundle.js'
+  },
+  module : {
+    loaders : [
+      {
+        test : /\.jsx?/,
+        include : APP_DIR,
+        exclude: /node_modules/,
+        loader : 'babel'
+      },
+      { test: /\.css$/, loader: "style-loader!css-loader!postcss-loader" },
+      { test: /\.json$/, loaders: ['json-loader'] },
+      {
+        test: /masonry|imagesloaded|fizzy\-ui\-utils|desandro\-|outlayer|get\-size|doc\-ready|eventie|eventemitter/,
+        loader: 'imports?define=>false&this=>window'
+      }
+    ]
+  },
+  postcss: function(webpack) {
+      return [
+        postcssImport({addDependencyTo: webpack}),
+        precss,
+        autoprefixer,
+        postcssMixins,
+        simpleVars,
+        colorFunctions,
+        postcssBEM,
+        postcssNested,
+        customMedia
+      ];
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    })
+  ]
+};
+
+module.exports = config;
