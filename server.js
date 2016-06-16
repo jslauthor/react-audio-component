80bd32b2cbd21f99d09ebaecf643ccd2bdd43b93
Leonard Souza <hey@leonardsouza.com>
2016-06-16 15:14:47 -0700

    switched hotloader to use 0.0.0.0

--- server.js
+++ server.js
@@ -7,10 +7,10 @@ new WebpackDevServer(webpack(config), {
   publicPath: config.output.publicPath,
   hot: true,
   historyApiFallback: true
-}).listen(3000, 'localhost', function (err, result) {
+}).listen(3000, '0.0.0.0', function (err, result) {
   if (err) {
     return console.log(err);
   }
 
-  console.log('Listening at http://localhost:3000/');
+  console.log('Listening at http://0.0.0.0:3000/');
 });
--- webpack.hot.js
+++ webpack.hot.js
@@ -16,7 +16,7 @@ const APP_DIR = path.resolve(__dirname, 'src');
 const config = {
   devtool: 'eval',
   entry: [
-    'webpack-dev-server/client?http://localhost:3000',
+    'webpack-dev-server/client?http://0.0.0.0:3000',
     'webpack/hot/only-dev-server',
     APP_DIR + '/index.js'
   ],
